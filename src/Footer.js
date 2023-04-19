53f557af431236ee760f1feb02a09144e6562a83
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2023-04-19 16:37:38 +0300

    Completed

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,26 @@
 import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
     <footer>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Products</a>
+        </li>
+        <li>
+          <a href="#">Contact Us</a>
+        </li>
+      </ul>
       <p>&copy; 2022 Little Lemon. All rights reserved.</p>
     </footer>
   );
